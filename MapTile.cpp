5d65e6ea6e47f950a1e261d58ddffd4cb088152b
Joy Mace <Jocelyn.Mace@colorado.edu>
2019-05-17 09:37:42 -0600

    Comments and removed duplicate constructor

--- MapTile.cpp
+++ MapTile.cpp
@@ -9,45 +9,61 @@ MapTile::~MapTile()
 {
     //dtor
 }
+
+// Make a map tile 
 MapTile::MapTile(bool status)
 {
     description = "None";
     gameOver = status;
-    item = false;
+    item_on = false;
 
 }
+
+// Make a map tile with description
 MapTile::MapTile(string d, bool s)
 {
     description = d;
     gameOver = s;
-    item = false;
+    item_on = false;
+}
 
-}
-MapTile::MapTile(string d, bool s)
-{
-    description = d;
-    gameOver = s;
-}
+// Set Description of Map tile
 void MapTile::setDescription(string S)
 {
     description = S;
 }
+
+// Get Description of Map tile
 string MapTile::getDescription()
 {
     return description;
 }
+
+// Is Baddie on Map tile
+// This will need to be changed as Map Tiles will need to be linked lists
+// Any items or Baddies will need to be held in the linked list as strings
+// So a bool telling whether there is a baddie is probably the wrong way to go
+// Instead, we should have a method that looks for the properties of a map tile
+// and in fact, this whole idea may be unnecessary depending on the rest of the code hahahaha
 void MapTile::setBaddie_on(bool b)
 {
     baddie_on = b;
 }
+
+// Is baddie on map tile
 bool MapTile::getBaddie_on()
 {
     return baddie_on;
 }
+
+// Set whether item is on map tile
+// Same as above, string name of item is a better approach
 void MapTile::setItem_on(bool I)
 {
     item_on = I;
 }
+
+// Get whether item is on map tile
 bool MapTile::getItem_on()
 {
     return item_on;
--- MapTile.h
+++ MapTile.h
@@ -11,17 +11,32 @@ using namespace std;
 class MapTile
 {
     public:
+		//consttructor and destructor
         MapTile();
         ~MapTile();
+
+		// is gameOver true?
         bool gameOver;
+
+		// Does this map tile contain an item?
         bool item_on;
+
+		//Does this map tile hide a baddie?
         bool baddie_on;
+
+		// two ways to make a map tile
         MapTile(bool);
         MapTile(string d, bool s);
+
+		// Set and get description of new map tile
         void setDescription(string s);
         string getDescription();
+
+		// Set and get  baddies on map tile
         void setBaddie_on(bool b);
         bool getBaddie_on();
+
+		// Set and get items on map tile
         void setItem_on(bool I);
         bool getItem_on();
 
